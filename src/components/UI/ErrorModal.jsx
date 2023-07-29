a847742c5d787e39c5f5d3687001c24294403e4b
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-07-28 21:50:54 -0300

    Creating portal in ErrorModal

--- src/components/UI/ErrorModal.jsx
+++ src/components/UI/ErrorModal.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import Card from './Card';
 import classes from './ErrorModal.module.css';
 import Button from './Button';
 
+const BackDrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+const ModalOverlay = (props) => {
+    return (
+        <Card>
+            <div className={classes.modal}>
+                <header className={classes.header}>
+                    <h2>{props.title}</h2>
+                </header>
+                <div className={classes.content}>   
+                    <p>{props.message}</p>
+                </div>
+                <footer className={classes.actions}>
+                    <Button onClick={props.onConfirm}>Ok</Button>
+                </footer>
+            </div>
+        </Card>
+    )
+}
+
 const ErrorModal = (props) => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm}>
-            <Card>
-                <div className={classes.modal}>
-                    <header className={classes.header}>
-                        <h2>{props.title}</h2>
-                    </header>
-                    <div className={classes.content}>
-                        <p>{props.message}</p>
-                    </div>
-                    <footer className={classes.actions}>
-                        <Button onClick={props.onConfirm}>Ok</Button>
-                    </footer>
-                </div>
-            </Card>
-            </div>
-        </div>
+        <>
+            {ReactDom.createPortal(<BackDrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))};
+            {ReactDom.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('modal-overlay-root'))};
+        </>
     )
 }
 
